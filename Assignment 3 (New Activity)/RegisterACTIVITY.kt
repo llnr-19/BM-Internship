106334c6dcae9db67f964e89e600362ba62e4e9a
Nooreldin Ayman <103221813+llnr-19@users.noreply.github.com>
2022-09-01 00:08:05 +0200

    Add files via upload

---
+++ Assignment 3 (New Activity)/RegisterACTIVITY.kt
@@ -0,0 +1,12 @@
+//Nooreldin Ayman
+package com.example.bminternship
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterACTIVITY : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
